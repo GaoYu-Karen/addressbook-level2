d414c725d1d29dd0dda3eb580b8443d75371e836
GaoYu-Karen <karen.gaoyu@gmail.com>
2016-09-08 09:03:50 +0800

    Use inheritance

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,25 @@
+package seedu.addressbook.data.person;
+
+public class Contact {
+    public final String value;
+    private boolean isPrivate;
+    
+    public Contact(String content, boolean isPricate) {
+        this.isPrivate = isPrivate;
+        this.value = content;
+    }
+    
+    @Override
+    public String toString() {
+        return value;
+    }
+    
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+    
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
